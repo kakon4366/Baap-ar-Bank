772aa9eb4ff7c013a1fcb2c39b5a66a57f7a800b
kakon4366 <kakonbarman4366@gmail.com>
2022-02-16 17:15:16 +0600

    User or Password added

--- js/bank-login.js
+++ js/bank-login.js
@@ -5,7 +5,7 @@ document.getElementById('bank-login-btn').addEventListener('click', function(){
     const errorMessage = document.getElementById('error-message');
 
     //check user or password
-    if(userEmail == 'admin@company.com' && userPassword === 'Admin'){
+    if(userEmail == 'user@gmail.com' && userPassword === 'Password'){
         window.location.href = 'banking.html';
     }else{
         errorMessage.style.visibility = 'visible';
